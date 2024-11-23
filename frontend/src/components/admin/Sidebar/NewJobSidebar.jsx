694947dfe3de51fa786127de2fca4f9fc9dd8eca
kanakk365 <kanakkumarmahala000@gmail.com>
2024-11-23 13:15:43 +0530

    margin issue fixed

--- frontend/src/components/admin/Sidebar/NewJobSidebar.jsx
+++ frontend/src/components/admin/Sidebar/NewJobSidebar.jsx
@@ -211,7 +211,7 @@ const Dashboard = () => {
   };
 
   return (
-    <div className=" mt-10 flex flex-1">
+    <div className=" mt-10 sm:mt-0 flex flex-1">
       <div className=" p-2  md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex items-center justify-center w-screen my-5">
           <form
